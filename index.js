dae4a5cec245c97bcd5f3242896a37fc4cdf75d3
Vansh Kumar <krvansh30@gmail.com>
2024-02-18 19:05:15 +0530

    changing origin

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const io = new Server({
   cors: {
-    origin: true,
+    origin: process.env.HOST,
     methods: ["GET", "POST"],
     credentials: true,
   },
