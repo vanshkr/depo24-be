e76e620e4838c96a946beaef23e6f37c884e2079
Vansh Kumar <krvansh30@gmail.com>
2024-02-18 18:53:49 +0530

    hiding cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(cors());
 app.use("/auth", authRoutes);
 app.use("/", roomRoutes);
 const PORT = process.env.PORT;
