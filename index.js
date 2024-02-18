2ea4593406e7171bf5bea3eacfae1aefeb01f389
Vansh Kumar <krvansh30@gmail.com>
2024-02-18 20:21:15 +0530

    cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const io = new Server({
   cors: {
-    origin: "https://depo24-fe.vercel.app/",
+    origin: "https://depo24-fe.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
